99cff0247207e45045f6fc7bd105bd399f2b9bed
yh9468 <younho9468@naver.com>
2018-11-23 21:18:58 +0900

    first init

--- MOHAGEE/app/src/main/java/com/example/maedin/mohagee/fragment/MypageFragmet.java
+++ MOHAGEE/app/src/main/java/com/example/maedin/mohagee/fragment/MypageFragmet.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.maedin.mohagee.R;
 
-public class MypageFragmet extends Fragment {
+public class MypageFragmet extends Fragment  implements View.OnClickListener {
 
     View view;
 
@@ -19,7 +21,27 @@ public class MypageFragmet extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_mypage, container, false);
+
+        ImageButton cemerabutton = view.findViewById(R.id.camera_button);
+        cemerabutton.setImageResource(R.drawable.camera);
+        cemerabutton.setOnClickListener(this);
+
         return view;
 
     }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.camera_button:
+
+                break;
+
+
+        }
+
+    }
+
 }
